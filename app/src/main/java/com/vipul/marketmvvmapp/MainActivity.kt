a99da0bd38a3f36622786543faa14e9f8cedb068
Vipul <vvipul229@gmail.com>
2022-03-28 19:12:20 +0530

    Init Commit

---
+++ app/src/main/java/com/vipul/marketmvvmapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vipul.marketmvvmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
